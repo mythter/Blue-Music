11bb2d43fadcf9e702a70eb8fe5e25b5bd5dbf58
Andrii Litvinov <andreoyy.litvinovv@gmail.com>
2023-10-21 17:56:41 +0300

    Added blazer project

---
+++ MusicPlayer/Spotify/Data/DataBaseMoq.cs
@@ -0,0 +1,6 @@
+﻿namespace Spotify.Data
+{
+    public class DataBaseMoq
+    {
+    }
+}
---
+++ MusicPlayer/Spotify/Data/Models/TrackModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Spotify.Data.Models
+{
+    public class TrackModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string Image { get; set; }
+
+    }
+}
