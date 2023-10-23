1044010d5ae7312e74d3b6bd2463d31c13f6fbbf
Andrii Litvinov <andreoyy.litvinovv@gmail.com>
2023-10-23 23:37:58 +0300

    Add TrackCard element and style

--- MusicPlayer/Spotify/Data/DataBaseMoq.cs
+++ MusicPlayer/Spotify/Data/DataBaseMoq.cs
@@ -1,6 +1,39 @@
-﻿namespace Spotify.Data
+﻿using Spotify.Data.Models;
+
+namespace Spotify.Data
 {
-    public class DataBaseMoq
+    public static class DataBaseMoq
     {
+        public static List<TrackModel> Tracks { get; set; }
+
+        static DataBaseMoq()
+        {
+            Tracks = new List<TrackModel>()
+            {
+               new TrackModel()
+               {
+                   Id = Guid.NewGuid(),
+                   Title = "Dr Manhattan",
+                   Author =  "Dr Manhattan",
+                   Image = "https://i.scdn.co/image/ab67616d0000b27386a8061cdf26909ce5a31c0c"
+               },
+               new TrackModel()
+               {
+                   Id = Guid.NewGuid(),
+                   Title = "Fight Or Flight",
+                   Author = "Hoobastank",
+                   Image = "https://i.scdn.co/image/ab67616d0000b273f2d4e8964965b4330ddc258f"
+               },
+               new TrackModel()
+               {
+                   Id = Guid.NewGuid(),
+                   Title = "Echoes, Silence, Patience & Grace",
+                   Author = "Foo Fighters",
+                   Image = "https://i.scdn.co/image/ab67616d0000b27383e260c313dc1ff1f17909cf"
+               }
+
+        };
+
+        }
     }
 }
--- MusicPlayer/Spotify/Data/Models/TrackModel.cs
+++ MusicPlayer/Spotify/Data/Models/TrackModel.cs
@@ -4,7 +4,6 @@
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
         public string Author { get; set; }
         public string Image { get; set; }
 
