b6058870555750904f1c21d4c1527abf6ed856da
Anton <anton260903@gmail.com>
2023-10-25 11:32:22 +0300

    playlist component added

--- MusicPlayer/Spotify/Data/DataBaseMoq.cs
+++ MusicPlayer/Spotify/Data/DataBaseMoq.cs
@@ -5,6 +5,7 @@ namespace Spotify.Data
     public static class DataBaseMoq
     {
         public static List<TrackModel> Tracks { get; set; }
+        public static List<PlaylistModel> Playlists { get; set; }
 
         static DataBaseMoq()
         {
@@ -31,8 +32,32 @@ namespace Spotify.Data
                    Author = "Foo Fighters",
                    Image = "https://i.scdn.co/image/ab67616d0000b27383e260c313dc1ff1f17909cf"
                }
+            };
 
-        };
+            Playlists = new List<PlaylistModel>()
+            {
+               new PlaylistModel()
+               {
+                   Id = Guid.NewGuid(),
+                   Title = "Playlist 1",
+                   Author =  "Kapusta",
+                   Image = "https://i.scdn.co/image/ab67616d0000b27386a8061cdf26909ce5a31c0c"
+               },
+               new PlaylistModel()
+               {
+                   Id = Guid.NewGuid(),
+                   Title = "Playlist 2",
+                   Author = "Ogurec",
+                   Image = "https://i.scdn.co/image/ab67616d0000b273f2d4e8964965b4330ddc258f"
+               },
+               new PlaylistModel()
+               {
+                   Id = Guid.NewGuid(),
+                   Title = "Playlist 3",
+                   Author = "Kaban",
+                   Image = "https://i.scdn.co/image/ab67616d0000b27383e260c313dc1ff1f17909cf"
+               }
+            };
 
         }
     }
---
+++ MusicPlayer/Spotify/Models/PlaylistModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Spotify.Models
+{
+    public class PlaylistModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Image { get; set; }
+    }
+}
