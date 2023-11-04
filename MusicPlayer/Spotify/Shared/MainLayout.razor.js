c150decbd1f09de186064cd8a2a1de1e50c48127
Anton <anton260903@gmail.com>
2023-11-04 22:39:44 +0200

    elements ids replaced with references

--- MusicPlayer/Spotify/Shared/MainLayout.razor.js
+++ MusicPlayer/Spotify/Shared/MainLayout.razor.js
@@ -1,9 +1,7 @@
 ﻿export class Main {
-    static Init(dotNet) {
+
+    static ResizeInit(resizer, sidebarPanel, favouritesPanel) {
         let resizing = false
-        let resizer = document.getElementById('resizer')
-        let resizingPanel = document.getElementById('sidebar')
-        let favouritesPanel = document.getElementById('favourites')
 
         resizer.addEventListener('mousedown', (e) => {
             resizing = true
@@ -14,12 +12,12 @@
         function resizingMove(event) {
             if (resizing) {
                 if (event.clientX <= 150) {
-                    resizingPanel.style.width = 74 + "px"
+                    sidebarPanel.style.width = 74 + "px"
                     favouritesPanel.style.paddingLeft = 5 + "px"
                     favouritesPanel.style.paddingRight = 5 + "px"
                 }
                 else {
-                    resizingPanel.style.width = event.clientX + "px"
+                    sidebarPanel.style.width = event.clientX + "px"
                     favouritesPanel.style.paddingLeft = 10 + "px"
                     favouritesPanel.style.paddingRight = 10 + "px"
                 }
@@ -34,16 +32,17 @@
             document.body.removeEventListener('mouseup', finishResizing)
             resizer.removeEventListener('mousemove', resizingMove)
         }
+    }
 
-        let main = document.getElementById("main")
-        main.addEventListener("scroll", (e) => {
-            const scrollTop = main.scrollTop
-            const scrollHeigth = main.scrollHeight
-            const clientHeight = main.clientHeight
+    static LoadNextInit(dotNet, mainPanel) {
+        mainPanel.addEventListener("scroll", () => {
+            const scrollTop = mainPanel.scrollTop
+            const scrollHeigth = mainPanel.scrollHeight
+            const clientHeight = mainPanel.clientHeight
             let percent = scrollTop * 100 / (scrollHeigth - clientHeight)
             console.log(scrollTop, scrollHeigth, clientHeight, "=>", scrollTop / (scrollHeigth - clientHeight))
 
-            if (percent > 90 /*&& scrollDown*/) {
+            if (percent > 90) {
                 dotNet.invokeMethodAsync("LoadNext")
             }
         })
