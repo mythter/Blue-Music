2b6eb06de3896bc12de7151966a1da216a6401e1
Anton <anton260903@gmail.com>
2023-11-22 16:28:50 +0200

    some comments to fix errors from new models

--- MusicPlayer/Spotify/Data/DataBaseMoq.cs
+++ MusicPlayer/Spotify/Data/DataBaseMoq.cs
@@ -15,8 +15,8 @@ namespace Spotify.Data
                {
                    Id = Guid.NewGuid(),
                    Title = "Dr Manhattan",
-                   Author =  "Dr Manhattan",
-                   Image = "https://i.scdn.co/image/ab67616d0000b27386a8061cdf26909ce5a31c0c",
+                   //Author =  "Dr Manhattan",
+                   //Image = "https://i.scdn.co/image/ab67616d0000b27386a8061cdf26909ce5a31c0c",
                    Source = "Find me.mp3",
 
                },
@@ -24,8 +24,8 @@ namespace Spotify.Data
                {
                    Id = Guid.NewGuid(),
                    Title = "Fight Or Flight",
-                   Author = "Hoobastank",
-                   Image = "https://i.scdn.co/image/ab67616d0000b273f2d4e8964965b4330ddc258f",
+                   //Author = "Hoobastank",
+                   //Image = "https://i.scdn.co/image/ab67616d0000b273f2d4e8964965b4330ddc258f",
                    Source = "Find me.mp3",
 
                },
@@ -33,8 +33,8 @@ namespace Spotify.Data
                {
                    Id = Guid.NewGuid(),
                    Title = "Echoes, Silence, Patience & Grace",
-                   Author = "Foo Fighters",
-                   Image = "https://i.scdn.co/image/ab67616d0000b27383e260c313dc1ff1f17909cf",
+                   //Author = "Foo Fighters",
+                   //Image = "https://i.scdn.co/image/ab67616d0000b27383e260c313dc1ff1f17909cf",
                    Source = "Find me.mp3",
 
                }
@@ -46,22 +46,22 @@ namespace Spotify.Data
                {
                    Id = Guid.NewGuid(),
                    Title = "Playlist 1",
-                   Author =  "Kapusta",
-                   Image = "https://i.scdn.co/image/ab67616d0000b27386a8061cdf26909ce5a31c0c"
+                   //Author =  "Kapusta",
+                   //Image = "https://i.scdn.co/image/ab67616d0000b27386a8061cdf26909ce5a31c0c"
                },
                new PlaylistModel()
                {
                    Id = Guid.NewGuid(),
                    Title = "Playlist 2",
-                   Author = "Ogurec",
-                   Image = "https://i.scdn.co/image/ab67616d0000b273f2d4e8964965b4330ddc258f"
+                   //Author = "Ogurec",
+                   //Image = "https://i.scdn.co/image/ab67616d0000b273f2d4e8964965b4330ddc258f"
                },
                new PlaylistModel()
                {
                    Id = Guid.NewGuid(),
                    Title = "Playlist 3",
-                   Author = "Kaban",
-                   Image = "https://i.scdn.co/image/ab67616d0000b27383e260c313dc1ff1f17909cf"
+                   //Author = "Kaban",
+                   //Image = "https://i.scdn.co/image/ab67616d0000b27383e260c313dc1ff1f17909cf"
                }
             };
 
