a16ef541d0725edf442b7d7abab791fb989b8032
Oyne <andreoyy.litvinovv@gmail.com>
2023-11-24 13:52:35 +0200

    Album card added

---
+++ MusicPlayer/Spotify/Pages/Items/AlbumCard.razor.js
@@ -0,0 +1,19 @@
+﻿export class PlaylistCard {
+    static InitHover(id) {
+        let target = document.getElementById(id);
+
+        target.addEventListener('mouseenter', function (e) {
+            e.target.style.setProperty('--x', e.offsetX + 'px');
+            e.target.style.setProperty('--y', e.offsetY + 'px');
+            e.target.style.setProperty('--w', (e.target.clientWidth * 4) + 'px');
+        });
+
+        target.addEventListener('mouseleave', function (e) {
+            let offsetX = e.offsetX > e.target.clientWidth ? e.offsetX - 50 : e.offsetX + 50;
+            let offsetY = e.offsetY > e.target.clientHeight ? e.offsetY + 50 : e.offsetY - 50;
+            e.target.style.setProperty('--x', offsetX + 'px');
+            e.target.style.setProperty('--y', offsetY + 'px');
+        });
+
+    }
+}
