7d3e6ba5489b0995fe0b8505d1179abca925b839
Anton <anton260903@gmail.com>
2023-11-25 23:40:52 +0200

    load next added to index

---
+++ MusicPlayer/Spotify/Pages/Index.razor.js
@@ -0,0 +1,15 @@
+﻿export class Index {
+
+    static LoadNextInit(dotNet, mainPanel) {
+        mainPanel.addEventListener("scroll", () => {
+            const scrollTop = mainPanel.scrollTop
+            const scrollHeigth = mainPanel.scrollHeight
+            const clientHeight = mainPanel.clientHeight
+            let percent = scrollTop * 100 / (scrollHeigth - clientHeight)
+
+            if (percent > 90) {
+                dotNet.invokeMethodAsync("LoadNext")
+            }
+        })
+    }
+}
