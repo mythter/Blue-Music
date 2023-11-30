15dc2bfd9db9db74d98bbe9ec326747d289066e4
Anton <anton260903@gmail.com>
2023-11-30 18:03:30 +0200

    PlayEventArgs added

---
+++ MusicPlayer/Spotify/Data/PlayEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using Spotify.Models;
+
+namespace Spotify.Data
+{
+    public class PlayEventArgs : EventArgs
+    {
+        public PlayEventArgs(List<TrackModel> trackList, int trackIndex)
+        {
+            TrackList = trackList;
+            TrackIndex = trackIndex;
+        }
+
+        public List<TrackModel> TrackList { get; set; }
+
+        public int TrackIndex { get; set; }
+    }
+}
