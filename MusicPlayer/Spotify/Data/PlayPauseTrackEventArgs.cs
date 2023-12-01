e1f685f073127a7c6067aef40eeb0a88073442ad
Anton <anton260903@gmail.com>
2023-12-01 20:55:12 +0200

    new event args added

---
+++ MusicPlayer/Spotify/Data/PlayPauseTrackEventArgs.cs
@@ -0,0 +1,21 @@
+﻿namespace Spotify.Data
+{
+    public class PlayPauseTrackEventArgs : EventArgs
+    {
+        public PlayPauseTrackEventArgs()
+        {
+        }
+
+        public PlayPauseTrackEventArgs(Guid collectionId, Guid trackId)
+        {
+            CollectionId = collectionId;
+            TrackId = trackId;
+        }
+
+        public Guid CollectionId { get; set; }
+
+        public Guid TrackId { get; set; }
+
+        public static readonly new PlayPauseTrackEventArgs Empty = new PlayPauseTrackEventArgs();
+    }
+}
---
+++ MusicPlayer/Spotify/Data/StartTrackEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using Spotify.Interfaces;
+
+namespace Spotify.Data
+{
+    public class StartTrackEventArgs : EventArgs
+    {
+        public StartTrackEventArgs()
+        {
+        }
+
+        public StartTrackEventArgs(ITrackStorable? trackCollection, int trackIndex)
+        {
+            TrackCollection = trackCollection;
+            TrackIndex = trackIndex;
+        }
+
+        public ITrackStorable? TrackCollection { get; set; }
+
+        public int TrackIndex { get; set; } = -1;
+
+        public static readonly new StartTrackEventArgs Empty = new StartTrackEventArgs();
+    }
+}
