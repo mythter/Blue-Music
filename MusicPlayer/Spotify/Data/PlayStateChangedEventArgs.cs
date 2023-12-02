55350ce3081c6c0466fcaab88a2988f8cc9b6a21
Anton <anton260903@gmail.com>
2023-12-02 15:08:52 +0200

    PlayStateChangedEventArgs added

---
+++ MusicPlayer/Spotify/Data/PlayStateChangedEventArgs.cs
@@ -0,0 +1,24 @@
+﻿namespace Spotify.Data
+{
+    public class PlayStateChangedEventArgs : EventArgs
+    {
+        public PlayStateChangedEventArgs()
+        {
+        }
+
+        public PlayStateChangedEventArgs(Guid collectionId, Guid trackId, bool isPaused)
+        {
+            CollectionId = collectionId;
+            TrackId = trackId;
+            IsPaused = isPaused;
+        }
+
+        public Guid CollectionId { get; set; }
+
+        public Guid TrackId { get; set; }
+
+        public bool IsPaused { get; set; }
+
+        public static readonly new PlayStateChangedEventArgs Empty = new PlayStateChangedEventArgs();
+    }
+}
