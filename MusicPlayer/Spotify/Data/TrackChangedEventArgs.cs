dd294acd1bbc45fc6adcca76a6ab1166f744a14f
Anton <anton260903@gmail.com>
2023-12-02 19:10:41 +0200

    TrackChangedEventArgs added

---
+++ MusicPlayer/Spotify/Data/TrackChangedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿namespace Spotify.Data
+{
+    public class TrackChangedEventArgs : EventArgs
+    {
+        public TrackChangedEventArgs()
+        {
+        }
+
+        public TrackChangedEventArgs(Guid collectionId, Guid trackId)
+        {
+            CollectionId = collectionId;
+            TrackId = trackId;
+        }
+
+        public Guid CollectionId { get; set; }
+
+        public Guid TrackId { get; set; }
+
+        public static readonly new TrackChangedEventArgs Empty = new TrackChangedEventArgs();
+    }
+}
