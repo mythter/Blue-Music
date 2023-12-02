7464d267a955f533a6838e99e1b4130fa3228ba5
Anton <anton260903@gmail.com>
2023-12-02 20:02:16 +0200

    null check in TrackCollectionCard js added

--- MusicPlayer/Spotify/Pages/Items/TrackCollectionCard.razor.js
+++ MusicPlayer/Spotify/Pages/Items/TrackCollectionCard.razor.js
@@ -1,17 +1,18 @@
 ﻿export class TrackCollectionCard {
     static InitHover(target) {
-        target.addEventListener('mouseenter', function (e) {
-            e.target.style.setProperty('--x', e.offsetX + 'px');
-            e.target.style.setProperty('--y', e.offsetY + 'px');
-            e.target.style.setProperty('--w', (e.target.clientWidth * 4) + 'px');
-        });
+        if (target != null) {
+            target.addEventListener('mouseenter', function (e) {
+                e.target.style.setProperty('--x', e.offsetX + 'px');
+                e.target.style.setProperty('--y', e.offsetY + 'px');
+                e.target.style.setProperty('--w', (e.target.clientWidth * 4) + 'px');
+            });
 
-        target.addEventListener('mouseleave', function (e) {
-            let offsetX = e.offsetX > e.target.clientWidth ? e.offsetX - 50 : e.offsetX + 50;
-            let offsetY = e.offsetY > e.target.clientHeight ? e.offsetY + 50 : e.offsetY - 50;
-            e.target.style.setProperty('--x', offsetX + 'px');
-            e.target.style.setProperty('--y', offsetY + 'px');
-        });
-
+            target.addEventListener('mouseleave', function (e) {
+                let offsetX = e.offsetX > e.target.clientWidth ? e.offsetX - 50 : e.offsetX + 50;
+                let offsetY = e.offsetY > e.target.clientHeight ? e.offsetY + 50 : e.offsetY - 50;
+                e.target.style.setProperty('--x', offsetX + 'px');
+                e.target.style.setProperty('--y', offsetY + 'px');
+            });
+        }
     }
 }
