364c9d2d61b517cb9b7457527847878f570b228a
Anton <anton260903@gmail.com>
2023-12-05 00:31:40 +0200

    some improvements

--- MusicPlayer/Spotify/Pages/Index.razor.js
+++ MusicPlayer/Spotify/Pages/Index.razor.js
@@ -1,15 +1,17 @@
 ﻿export class Index {
 
     static LoadNextInit(dotNet, mainPanel) {
-        mainPanel.addEventListener("scroll", () => {
-            const scrollTop = mainPanel.scrollTop
-            const scrollHeigth = mainPanel.scrollHeight
-            const clientHeight = mainPanel.clientHeight
-            let percent = scrollTop * 100 / (scrollHeigth - clientHeight)
+        if (mainPanel != null) {
+            mainPanel.addEventListener("scroll", () => {
+                const scrollTop = mainPanel.scrollTop
+                const scrollHeigth = mainPanel.scrollHeight
+                const clientHeight = mainPanel.clientHeight
+                let percent = scrollTop * 100 / (scrollHeigth - clientHeight)
 
-            if (percent > 75) {
-                dotNet.invokeMethodAsync("LoadNext")
-            }
-        })
+                if (percent > 75) {
+                    dotNet.invokeMethodAsync("LoadNext")
+                }
+            })
+        }
     }
 }
